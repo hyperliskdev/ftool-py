43c084c203436b4184bc43630713ea1779bb231b
Carter Tomlenovich <cartertomlenovich@gmail.com>
2025-10-02 13:29:53 -0400

    dotenv

--- ftoolpy/auth.py
+++ ftoolpy/auth.py
@@ -3,9 +3,15 @@ import dotenv
 
 def get_client():
     api = dotenv.load_dotenv(".env")
-    client_id = api.get("FALCON_CLIENT_ID")
-    client_secret = api.get("FALCON_CLIENT_SECRET")
+
+    if api is False:
+        raise Exception("Failed to load .env file")
+    
+    client_id = dotenv.get_key(".env", "FALCON_CLIENT_ID")
+    client_secret = dotenv.get_key(".env", "FALCON_CLIENT_SECRET")
+
     if not client_id or not client_secret:
-        raise ValueError("FALCON_CLIENT_ID and FALCON_CLIENT_SECRET must be set in the .env file")
+        raise Exception("FALCON_CLIENT_ID or FALCON_CLIENT_SECRET not found in .env file")
+
 
     return APIHarnessV2(client_id=client_id, client_secret=client_secret)
